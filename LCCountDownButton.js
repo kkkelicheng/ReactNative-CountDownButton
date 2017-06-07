a285c985524ed216f83a059985dabcdca8379130
kelicheng <kelicheng131@gmail.com>
2017-06-07 16:45:53 +0800

    first init

---
+++ LCCountDownButton.js
@@ -0,0 +1,128 @@
+/**
+ * Created by lichengke on 2017/6/6.
+ */
+
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity
+} from 'react-native';
+
+const LCCountDownButtonState = {
+    LCCountDownButtonActive : 0,
+    LCCountDownButtonDisable : 1,
+}
+
+var timeRecodes = [];  //以后用
+
+export default class LCCountDownButton extends Component {
+
+    // 构造
+      constructor(props) {
+        super(props);
+        // 初始状态
+          this.state={
+             btnTitle:'默认'
+          }
+      }
+
+    static propTypes = {
+        id:React.PropTypes.string,          //以后用
+        beginText:React.PropTypes.string,   //初始状态按钮title
+        endText:React.PropTypes.string,     //读秒结束后按钮的title
+        count:React.PropTypes.number,       //计时数
+        pressAction:React.PropTypes.func,   //按下按钮的事件,但是触发倒数需要你自己来调用方法
+        changeWithCount:React.PropTypes.func,   //读秒变化的函数,该函数带有一个参数count,表示当前的剩余事件
+        end:React.PropTypes.func,           //读秒完毕后的函数
+    }
+
+    buttonState = LCCountDownButtonState.LCCountDownButtonActive;
+
+    componentWillMount() {
+        this.state = {
+            btnTitle:this.props.beginText,
+        }
+    }
+
+    clearTime(){
+        this.interval && clearInterval(this.interval);
+    }
+
+    componentWillUnMount() {
+        this.clearTime();
+    }
+
+    //外界调用
+    startCountDown(){
+        this.buttonState = LCCountDownButtonState.LCCountDownButtonDisable;
+        const {changeWithCount,endText,count,end}= this.props;
+        this.startTime = Date.now();
+        this.interval = setInterval(()=>{
+            let detalTime = Math.round((Date.now() - this.startTime)/1000);
+            let content = changeWithCount(count - detalTime);
+            if (detalTime >= count){
+                content = endText;
+                this.clearTime();
+                end && end();
+                this.buttonState = LCCountDownButtonState.LCCountDownButtonActive;
+            }
+            this.setState({
+                btnTitle:content
+            })
+        },1000)
+    }
+
+    render(){
+        let isDisable = this.buttonState == LCCountDownButtonState.LCCountDownButtonDisable
+        return (
+            <TouchableOpacity disabled={isDisable}
+                              onPress={()=>{this.props.pressAction && this.props.pressAction()}}
+                              style={[styles.buttonCommonStyle,isDisable?styles.disableButtonStyle:styles.activeButtonStyle]}
+            >
+                <Text style={[styles.txtCommonStyle,isDisable?styles.disableTxtStyle:styles.activeTxtStyle]}>
+                    {this.state.btnTitle}
+                </Text>
+            </TouchableOpacity>
+        );
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+
+    buttonCommonStyle:{
+        borderRadius:3,
+        borderWidth:1,
+        borderColor:'gray',
+        paddingRight:15,
+        paddingLeft:15,
+        paddingTop:8,
+        paddingBottom:8,
+    },
+    //禁用时候的TouchableOpacity样式
+    disableButtonStyle:{
+        backgroundColor:'red',
+    },
+    //可以点击时候的TouchableOpacity样式
+    activeButtonStyle:{
+        backgroundColor:'green',
+    },
+
+    txtCommonStyle:{
+        fontSize:14,
+    },
+    //禁用时候的Text样式
+    disableTxtStyle:{
+        color:'gray',
+    },
+    //可以点击时候的Text样式
+    activeTxtStyle:{
+        color:'black',
+    }
+});
--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # ReactNative-CountDownButton
 封装的一个倒计时按钮
+为啥自己写一个，感觉别人写的不好而已
+
+## 特点
+* 容易自己改代码
+* 接口多
+
+
+## 怎么使用
+直接下载该js文件就行了，不需要npm xxx这东西
+
+**1. 初始化该组件**
+
+```
+
+// changeWithCount 是返回一个字符串表示button的title
+// pressAction  按下按钮的事件，并不会触发倒计时
+ <LCCountDownButton beginText={'点击获取验证码'} 
+                    endText={'再次点击获取验证码'}
+                    changeWithCount={(count)=>count+'秒后获取'}
+                    count={5}
+                    end={()=>{console.log('读秒结束')}}
+                    pressAction={()=>{this._countDownButtonPressed()}}
+                    ref="LCCountDownButton"
+
+/>
+
+```
+
+**2.触发LCCountDownButton倒计时方法**
+
+```
+// 按下按钮触发
+    _countDownButtonPressed(){
+    		// 1s 后触发倒计时
+        this.timer = setTimeout(this.triggerCount.bind(this),1000);
+    }
+
+// 拿到按钮，开始倒计时
+    _triggerCount(){
+        let button = this.refs.LCCountDownButton;
+        button.startCountDown();
+    }
+
+```
+
+## 说一说基本的构成
+很简单一个按钮而已,用`TouchableOpacity`和`Text`做的一个轮子而已
+
+## 交给你做的事 
+TouchableOpacity 和 Text 不同状态下的样式，在源文件里面已经写了。
+当然你也可以自己改源文件，将这些样式作为属性传入。
+
+## 效果图
