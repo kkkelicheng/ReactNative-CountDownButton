b74e8c3e02d22e11189bfdbe0576ea6b1f053e81
kelicheng <kelicheng131@gmail.com>
2017-06-13 14:58:08 +0800

    支持下次进入该，智能显示是不是在读秒

--- LCCountDownButton.js
+++ LCCountDownButton.js
@@ -1,9 +1,9 @@
 /**
- * Created by lichengke on 2017/6/6.
+ * Created by lichengke on 2017/6/12.
  */
 
 
-import React, { Component ,PropTypes} from 'react';
+import React, { Component , PropTypes} from 'react';
 import {
     AppRegistry,
     StyleSheet,
@@ -18,51 +18,76 @@ const LCCountDownButtonState = {
     LCCountDownButtonDisable : 1,
 }
 
-var timeRecodes = [];  //以后用
+// {id , startTime, deathCount}
+var timeRecodes = [];  //根据id来记录LCCountDownButton的信息
 
 export default class LCCountDownButton extends Component {
 
     // 构造
-      constructor(props) {
+    constructor(props) {
         super(props);
         // 初始状态
-          this.state={
-             btnTitle:'默认'
-          }
-      }
+        this.state={
+            btnTitle:'默认'
+        }
+    }
 
     static propTypes = {
-        id               :React.PropTypes.string,          //以后用
-        beginText        :React.PropTypes.string,   //初始状态按钮title
-        endText          :React.PropTypes.string,     //读秒结束后按钮的title
-        count            :React.PropTypes.number,       //计时数
-        pressAction      :React.PropTypes.func,   //按下按钮的事件,但是触发倒数需要你自己来调用方法
-        changeWithCount  :React.PropTypes.func,   //读秒变化的函数,该函数带有一个参数count,表示当前的剩余事件
-        end              :React.PropTypes.func,           //读秒完毕后的函数
-        frameStyle       :View.propTypes.style    //初始化的位置大小
+        id:React.PropTypes.string,          //按钮的身份标识,同一个页面的按钮是同一个id
+        beginText:React.PropTypes.string,   //初始状态按钮title
+        endText:React.PropTypes.string,     //读秒结束后按钮的title
+        count:React.PropTypes.number,       //计时数
+        pressAction:React.PropTypes.func,   //按下按钮的事件,但是触发倒数需要你自己来调用方法
+        changeWithCount:React.PropTypes.func,   //读秒变化的函数,该函数带有一个参数count,表示当前的剩余事件
+        end:React.PropTypes.func,           //读秒完毕后的函数
+        frameStyle:View.propTypes.style    //初始化的位置大小
     }
 
     buttonState = LCCountDownButtonState.LCCountDownButtonActive;
 
     componentWillMount() {
+        this.shouldSetState = true;
         this.state = {
             btnTitle:this.props.beginText,
         }
     }
 
-    clearTime(){
-        this.interval && clearInterval(this.interval);
+    componentDidMount() {
+        const {id,changeWithCount} = this.props;
+        for(var i = 0 ; i<timeRecodes.length ; i ++){
+            let obj = timeRecodes[i];
+            if (obj.id == id){
+                let liveTime = Date.now() - obj.startTime
+                if (liveTime < obj.deathCount * 1000){
+                    //避免闪动
+                    let detalTime = Math.round(liveTime/1000);
+                    let content = changeWithCount(obj.deathCount - detalTime);
+                    this.setState({
+                        btnTitle:content
+                    });
+                    //手动调用倒计时
+                    this.startCountDownWithCount(obj.startTime)
+                }
+            }
+        }
+
     }
 
-    componentWillUnMount() {
+    clearTime(){
+        if (this.interval){
+            clearInterval(this.interval)
+        }
+    }
+
+    componentWillUnmount() {
+        this.shouldSetState = false;
         this.clearTime();
     }
 
-    //外界调用
-    startCountDown(){
+    startCountDownWithCount(startTime){
         this.buttonState = LCCountDownButtonState.LCCountDownButtonDisable;
         const {changeWithCount,endText,count,end}= this.props;
-        this.startTime = Date.now();
+        this.startTime = startTime;
         this.interval = setInterval(()=>{
             let detalTime = Math.round((Date.now() - this.startTime)/1000);
             let content = changeWithCount(count - detalTime);
@@ -72,16 +97,44 @@ export default class LCCountDownButton extends Component {
                 end && end();
                 this.buttonState = LCCountDownButtonState.LCCountDownButtonActive;
             }
-            this.setState({
-                btnTitle:content
-            })
+            if(this.shouldSetState){
+                this.setState({
+                    btnTitle:content
+                })
+            }
         },1000)
     }
 
-    render(){
-        let isDisable = this.buttonState == LCCountDownButtonState.LCCountDownButtonDisable
-        const {frameStyle} = this.props
+    recordButtonInfo(){
+        const {id , count} = this.props;
+        var hasRecord = false;
+        for (var i = 0 ; i < timeRecodes.length ; i ++){
+            let obj = timeRecodes[i];
+            if(obj.id == id){
+                obj.startTime = Date.now();
+                hasRecord = true;
+                break;
+            }
+        }
+        if (!hasRecord){
+            let buttonInfo = {
+                id:id,
+                deathCount:count,
+                startTime:Date.now()
+            }
+            timeRecodes.push(buttonInfo)
+        }
+    }
 
+    //外界调用
+    startCountDown(){
+        this.startCountDownWithCount(Date.now());
+        this.recordButtonInfo();
+    }
+
+    render(){
+        let isDisable = this.buttonState == LCCountDownButtonState.LCCountDownButtonDisable;
+        const {frameStyle} = this.props
         return (
             <TouchableOpacity disabled={isDisable}
                               onPress={()=>{this.props.pressAction && this.props.pressAction()}}
@@ -103,8 +156,8 @@ const styles = StyleSheet.create({
         borderRadius:3,
         borderWidth:1,
         borderColor:'gray',
-        paddingRight:15,
-        paddingLeft:15,
+        paddingRight:8,
+        paddingLeft:8,
         paddingTop:8,
         paddingBottom:8,
         justifyContent:'center',
--- README.md
+++ README.md
@@ -1,42 +1,62 @@
 # ReactNative-CountDownButton
 封装的一个倒计时按钮,仅仅使用`TouchableOpacity`和`Text`两个东西
 
+为啥自己写一个? 感觉别人写的不好而已，而且功能不全.
 
-为啥自己写一个? 感觉别人写的不好而已
+有好的建议请给issue，有问题也可以提出。
+
+有个RN的交流群397885169，我是click，加群也可以找到我
 
 ## 特点
-* 容易自己改代码
-* 接口多
+* **容易自己改代码**
 
+	仅仅使用`TouchableOpacity`和`Text`两个东西，样式你得自己写
 
+* **接口多**
+
+	回调什么的都有，可以见源码的`propTypes`有介绍
+	
+* **其他的2个优点**
+	
+	1. 不会因为进入后台而停止读秒
+	2. 支持同个页面再次进入时，智能的判断读秒时间，显示是否继续计时
+	
+	
+	
 ## 怎么使用
 直接下载该js文件就行了，不需要npm xxx这东西
 
 **1. 初始化该组件**
 
+不同的页面LCCountDownButton的属性id要设置不同，因为‘智能的判断读秒时间’需要id来识别
+
+
 ```
 
 // changeWithCount 是返回一个字符串表示button的title
 // pressAction  按下按钮的事件，并不会触发倒计时
- <LCCountDownButton beginText = {'点击获取验证码'} 
-                    endText = {'再次点击获取验证码'}
-                    changeWithCount = {(count)=>count+'秒后获取'}
-                    count = {5}
-                    end = {()=>{console.log('读秒结束')}}
-                    pressAction = {()=>{this._countDownButtonPressed()}}
-                    ref = "LCCountDownButton"
-                    frameStyle = {{width:200,height:40,position:'absolute',right:10,top:100}}
 
-/>
+<LCCountDownButton frameStyle={{top:44 * 3 + 4,right:10,width:120,height:36,position:'absolute'}}
+                   beginText='获取验证码'
+                   endText='再次获取验证码'
+                   count={10}
+                   pressAction={()=>{this.countDownButton.startCountDown()}}
+                   changeWithCount={(count)=> count + 's后重新获取'}
+                   id='register'   
+                   ref={(e)=>{this.countDownButton=e}}
+                   />
 
 ```
 
 **2.触发LCCountDownButton倒计时方法**
 
+要主动调用`startCountDown`这个方法，按钮才会读秒
+
+
 ```
-// 按下按钮触发
+// 这个方法就是上面pressAction触发
     _countDownButtonPressed(){
-    		// 1s 后触发倒计时
+    		// 1s 后触发倒计时  例如做网络请求后的再读秒
         this.timer = setTimeout(this.triggerCount.bind(this),1000);
     }
 
